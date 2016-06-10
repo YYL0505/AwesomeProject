2ee55fba07713a95e11d155ceed27778f1eb4935
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-10 14:54:43 +0100

    YL - render a movie

--- index.android.js
+++ index.android.js
@@ -4,49 +4,62 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var MOCKED_MOVIES_DATA = [
+    {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+
+class AwesomeProject extends Component {
+    render() {
+        var movie = MOCKED_MOVIES_DATA[0];
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    }
+}
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
