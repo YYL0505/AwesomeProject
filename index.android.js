2d73031fdcdf573e8279edb96802c2124017b602
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-11 13:04:55 +0100

    YL - add action bar for the shot

--- index.android.js
+++ index.android.js
@@ -74,25 +74,84 @@ class AwesomeProject extends Component {
                     source={{uri: movie.posters.thumbnail}}
                     style={styles.thumbnail}
                 />
+
+                <View style={styles.shotActionBar}>
+                    <Text style={styles.shotActionBarViewCountImage}>
+                        view
+                    </Text>
+                    <Text style={styles.shotActionBarViewCount}>
+                        10
+                    </Text>
+
+                    <Text style={styles.shotActionBarLikeCountImage}>
+                        like
+                    </Text>
+                    <Text style={styles.shotActionBarLikeCount}>
+                        8
+                    </Text>
+
+                    <Text style={styles.shotActionBarCommentCountImage}>comment</Text>
+                    <Text style={styles.shotActionBarCommentCount}>5</Text>
+                </View>
             </View>
         );
     }
 }
 var styles = StyleSheet.create({
     container: {
+        flexWrap:'wrap',
+        flexDirection:'row',
         backgroundColor: '#ffffff',
         justifyContent: 'center',
         alignItems: 'center',
         padding: 10,
         width: 150,
-        height: 150,
+        height: 120,
         marginTop: 10,
         marginRight: 10,
     },
     thumbnail: {
-        width: 100,
-        height: 100,
+        width: 120,
+        height: 90,
     },
+    shotActionBar: {
+        flexWrap:'wrap',
+        flexDirection:'row',
+        justifyContent: 'flex-end',
+        width: 120,
+        marginTop: 10,
+    },
+    shotActionBarViewCountImage: {
+        fontSize: 8,
+        textAlign: 'center',
+        marginRight:2,
+    },
+    shotActionBarViewCount: {
+        fontSize: 8,
+        textAlign: 'center',
+        marginRight:5,
+    },
+    shotActionBarLikeCountImage: {
+        fontSize: 8,
+        textAlign: 'center',
+        marginRight:2,
+    },
+    shotActionBarLikeCount: {
+        fontSize: 8,
+        textAlign: 'center',
+        marginRight:5,
+    },
+    shotActionBarCommentCountImage: {
+        fontSize: 8,
+        textAlign: 'center',
+        marginRight:2,
+    },
+
+    shotActionBarCommentCount: {
+        fontSize: 8,
+        textAlign: 'center',
+    },
+
     listView: {
         backgroundColor: '#F4F4F4',
         paddingLeft: 20,
